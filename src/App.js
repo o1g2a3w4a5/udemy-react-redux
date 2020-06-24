608fb8b6a3b0b8d2aa0bfa2160fab29466239eed
“o1g2a3w4a5” <o1g2a3w4a5@gmail.com>
2020-06-24 21:32:04 +0900

    ok

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+import React, { Component } from 'react';
+
+// class App extends Comment {
+//     render() {
+//         return <div><p>Hello-world</p></div>;
+//     }
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello-world
-        </a>
-      </header>
-    </div>
-  );
+    return <div><p>Hello-world</p></div>;
 }
 
 export default App;
